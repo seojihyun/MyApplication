f3c40305be8af09bc644141487fbd39319c3bec5
seojihyun <seojihyunn@naver.com>
2015-11-26 23:11:32 +0900

    dialog

---
+++ dialog/src/main/java/com/example/seojihyun/myapplication/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.seojihyun.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
+
+        tvName = (TextView)findViewById(R.id.tvName);
+        tvEmail = (TextView)findViewById(R.id.tvEmail);
+        button1 = (Button)findViewById(R.id.button1);
+
+        //버튼 리스너 부착
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.ic_menu_allfriends);
+                dlg.setView(dialogView);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dlgEdtName = (EditText) dialogView.findViewById(R.id.UserName);
+                        dlgEdtEmail = (EditText) dialogView.findViewById(R.id.UserEmail);
+
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast toast = new Toast(MainActivity.this);
+                        toastView = (View) View.inflate(MainActivity.this, R.layout.toast1, null);
+                        toastText = (TextView) toastView.findViewById(R.id.toastText);
+                        toastText.setText("취소 했습니다");
+                        toast.setView(toastView);
+                        /*랜덤 위치 설정*/
+                        Random random = new Random();
+                        int x = random.nextInt(100);
+                        int y = random.nextInt(300);
+                        toast.setGravity(Gravity.CENTER, x, y);
+
+                        toast.show();
+                    }
+                });
+                dlgEdtName = (EditText)dialogView.findViewById(R.id.UserName);
+                dlgEdtEmail = (EditText)dialogView.findViewById(R.id.UserEmail);
+
+                dlgEdtName.setText(tvName.getText().toString());
+                dlgEdtEmail.setText(tvEmail.getText().toString());
+                dlg.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
