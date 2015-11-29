58cc8f0daba50b8791f5a925ecfca5be1818dae7
seojihyun <seojihyunn@naver.com>
2015-11-29 23:20:52 +0900

    일기장 앱 만들기

--- seoDiary/src/main/java/com/example/seojihyun/myapplication/MainActivity.java
+++ seoDiary/src/main/java/com/example/seojihyun/myapplication/MainActivity.java
@@ -228,14 +228,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
 
         switch (id) {
